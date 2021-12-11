6d859021af3f45fce3fee4dcf1d4cddaf52f005f
Surya <surya@Suryas-MacBook-Air.local>
2021-12-11 18:19:13 -0500

    Implementation for getting the predictions [updating the regex for all the possible delimiters is pending]

--- n_gram_parser.py
+++ n_gram_parser.py
@@ -1,34 +1,91 @@
+from collections import defaultdict
 import re
+from typing import OrderedDict
 from fastapi.params import File
 
 
 class NGramParser:
     n: int
     file: File
-    out_put: dict
+    frequency_map: defaultdict(list)
+    prediction_map: defaultdict(list)
     words: list
 
     def __init__(self, n: int, file: File):
         self.n = n
         self.file = file
-        self.out_put = {}
+        self.frequency_map = defaultdict(lambda:1)
         self.words = []
+        self.prediction_map = defaultdict(list)
+    
+    """
+    This method returns the frequency and prediction maps for the entire file
+    """
+    def get_frequency_and_predictions(self) -> dict:
+        self.run()
+        return {
+            "Frequencies": self.frequency_map,
+            "Predictions": self.prediction_map
+        }
+    
+    """
+    This method will returns the number of occurances of a given text.
+    """
+    def get_frequency_of_text(self, text: str) -> dict:
+        self.run()
+        result = {
+            f'Frequency of [{text.lower()}]': 0
+        }
+        if text.lower() in self.frequency_map:
+            result[f'Frequency of [{text.lower()}]'] = self.frequency_map[text.lower()]
+        return result
+    
+    """
+    This method will get x number of predictions based on highest number of occurances.
+    1. If all the words have same occurances we will just choose top x items from the list.
+    2. Prediction are based on n number of words list created.
+    """
+    def get_predictions(self, x: int, text: str) -> dict:
+        self.run()
+        result = {
+            f'candidate completions for [{text.lower()}]': {}
+        }
+        if text.lower() in self.prediction_map:
+            next_words_set = set(self.prediction_map[text.lower()])
+            next_words = self.prediction_map[text.lower()]
+            predictions = {}
+            for item in next_words_set:
+                predictions[item] = next_words.count(item)
+            predictions = sorted(predictions.items(), key=lambda x: x[1], reverse=True)
+            predictions = predictions[:x] if x < len(predictions) else predictions
+            result[f'candidate completions for [{text.lower()}]'] = dict(predictions)
+        return result
 
-    def run(self) -> dict:
+    """
+    This method is just orchestrating the flow of data.
+    """
+    def run(self):
         self.parse_data()
         self.frequency_collector()
-        return self.out_put
 
+    """
+    This method is parsing the file and creating a list with all the words in the file.
+    We are using regex to use multiple delimiters to split the text.
+    """
     def parse_data(self):
         file_content = self.file.file.readlines()
         for line in file_content:
-            # \.\+,:;-_\"$\|
             content = line.decode("utf-8")
             content = content.replace("\r\n", "")
-            self.words.extend(re.split(r"[ \.,]", content))
+            self.words.extend(re.split(r"[ \.,!\[\]:#-*]", content))
         while "" in self.words:
             self.words.remove("")
     
+    """
+    This method is iterating through the words list generated from the file and creating map for the following:
+    Frequency of word sequence based on provided n (number of words)
+    Predictions of next word that occurs after a given sequence
+    """
     def frequency_collector(self):
         start_index = 0
         end_index = self.n
@@ -37,9 +94,7 @@ class NGramParser:
             for i in range(start_index, end_index):
                 current_words.append(self.words[i].lower())
                 current_string = " ".join(current_words)
-                if current_string in self.out_put:
-                    self.out_put[current_string] += 1
-                else:
-                    self.out_put[current_string] = 1
+                if i != len(self.words)-1:
+                    self.prediction_map[current_string].append(self.words[i+1].lower())
+                self.frequency_map[current_string] += 1
             self.words.pop(0)
-
